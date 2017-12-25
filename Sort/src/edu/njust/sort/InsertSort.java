a2895c811b3d89d9488de5538a72124047675c49
keyuan.dky <keyuan.dky@alibaba-inc.com>
2017-12-25 11:28:17 +0800

    1

--- Sort/src/edu/njust/sort/InsertSort.java
+++ Sort/src/edu/njust/sort/InsertSort.java
@@ -10,13 +10,18 @@ public class InsertSort extends Sort{
 		for(int i=1;i<len;i++){
 			int temp=array[i];
 			int j=i;
-			if(array[j-1]>temp){
+/*			if(array[j-1]>temp){
 				while (j>=1&&array[j-1]>temp) {
 					array[j]=array[j-1];
 					j--;
 				}
 				array[j]=temp;	
+			}*/
+			while (j>=1&&array[j-1]>temp){
+				array[j]=array[j-1];
+				j--;
 			}
+			array[j]=temp;
 		
 			
 		}
